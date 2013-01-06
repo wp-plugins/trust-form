a866089f2e9284a81f651e685609574f23f9017b
horike <horike@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-06 20:41:00 +0000

    tagging version 1.5.1
    git-svn-id: https://plugins.svn.wordpress.org/trust-form@648887 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.5.1/admin/paypal.php
@@ -0,0 +1,15 @@
+<div id="paypal-footer">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="horike37@gmail.com">
+<input type="hidden" name="lc" value="US">
+<input type="hidden" name="item_name" value="Trust Form">
+<input type="hidden" name="button_subtype" value="services">
+<input type="hidden" name="no_note" value="0">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_SM.gif:NonHostedGuest">
+<p><?php _e( 'Trust Form is in need of your support. Please donate in order to continue development.', TRUST_FORM_DOMAIN ); ?></p>
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/ja_JP/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div>
